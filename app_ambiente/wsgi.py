871053782ecd918007964aba12f00904f70c9a76
Adriann Sanchez <alka65@hotmail.com>
2016-06-04 16:41:40 -0500

    heroku

--- app_ambiente/wsgi.py
+++ app_ambiente/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_ambiente.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_ambiente.settings")
+
 application = get_wsgi_application()
 
 try:
